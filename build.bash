0b0152f053cb64a4ecb9190b2e23533814ec6874
oov <ilovevipper@gmail.com>
2025-09-15 21:21:55 +0900

    update ovbase

--- build.bash
+++ build.bash
@@ -44,7 +44,7 @@ done
 if [ "${INSTALL_TOOLS}" -eq 1 ]; then
   mkdir -p "build/tools"
   if [ ! -e "${PWD}/build/tools/setup-llvm-mingw.sh" ]; then
-    curl -o "${PWD}/build/tools/setup-llvm-mingw.sh" -sOL https://raw.githubusercontent.com/oov/ovbase/d59e6ce13417ba77b19b8a964b751d232b857de4/setup-llvm-mingw.sh
+    curl -o "${PWD}/build/tools/setup-llvm-mingw.sh" -sOL https://raw.githubusercontent.com/oov/ovbase/9558fff1d6c837b6eff9c3b7a23caf9f2923d708/setup-llvm-mingw.sh
   fi
   . "${PWD}/build/tools/setup-llvm-mingw.sh" --dir "${PWD}/build/tools"
 fi
